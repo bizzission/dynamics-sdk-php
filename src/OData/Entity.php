ea06c2528d0b77850ff6616c3e497ba498cb7f8f
Peter DeMarco <pdemarco@linkedin.com>
2018-03-15 13:53:07 -0500

    Change Entity->primaryKey to static.

--- src/OData/Entity.php
+++ src/OData/Entity.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * Copyright (c) Saint Systems, LLC.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
-* 
+*
 * OData Entity File
 * PHP version 7
 *
@@ -47,8 +47,8 @@ class Entity implements ArrayAccess
      *
      * @var string
      */
-    protected $primaryKey = 'id';
-     
+    static $primaryKey = 'id';
+
     /**
      * The "type" of the entity key.
      * @var string
